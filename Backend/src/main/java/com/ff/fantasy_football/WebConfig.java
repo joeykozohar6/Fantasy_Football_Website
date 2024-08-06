fbbaff3a51c0b91f1dce97564f9b1e248b8509bb
joeykozohar <joeykozohar@vt.edu>
2024-08-06 09:52:49 -0400

    Add webconfig file

---
+++ Backend/src/main/java/com/ff/fantasy_football/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ff.fantasy_football;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow all paths
+                .allowedOrigins("http://localhost:3000")  // Allow this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow these HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);  // Allow credentials
+    }
+}
